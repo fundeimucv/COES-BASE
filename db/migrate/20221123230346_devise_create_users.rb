5aa90809fff8a59530e162be0c198e352c415579
Daniel Moros <danielousky@gmail.com>
2022-11-24 19:32:30 -0400

    Added full users fields

--- db/migrate/20221123230346_devise_create_users.rb
+++ db/migrate/20221123230346_devise_create_users.rb
@@ -5,21 +5,31 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :ci, null: false
       t.string :encrypted_password, null: false, default: ""
+      t.string :name
+      t.string :last_name
+      t.string :number_phone
+      t.integer :sex
+      t.integer :nacionality
+      t.integer :marital_status
+      t.date :birth_date
+      t.string :origin_country
+      t.string :origin_city
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -37,7 +47,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :ci,                unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,27 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_230346) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "ci", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string "name"
+    t.string "last_name"
+    t.string "number_phone"
+    t.integer "sex"
+    t.integer "nacionality"
+    t.integer "marital_status"
+    t.date "birth_date"
+    t.string "origin_country"
+    t.string "origin_city"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["ci"], name: "index_users_on_ci", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
