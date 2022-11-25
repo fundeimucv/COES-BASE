659da7f68eb28ae5622c6b3bdfed2a50b2dca577
Daniel Moros <danielousky@gmail.com>
2022-11-25 18:31:46 -0400

    Added Student model

---
+++ db/migrate/20221125153533_create_students.rb
@@ -0,0 +1,18 @@
+class CreateStudents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :students, id: false do |t|
+      t.references :user, null: false, foreign_key: true, primary_key: true
+      t.boolean :active, default: true
+      t.integer :disability
+      t.integer :nacionality
+      t.integer :marital_status
+      t.string :origin_country
+      t.string :origin_city
+      t.date :birth_date
+
+      t.timestamps
+    end
+    add_foreign_key :students, :users, column: :user_id, on_delete: :cascade, on_update: :cascade
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_230346) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "ci", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "name"
-    t.string "last_name"
-    t.string "number_phone"
-    t.integer "sex"
-    t.integer "nacionality"
-    t.integer "marital_status"
-    t.date "birth_date"
-    t.string "origin_country"
-    t.string "origin_city"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["ci"], name: "index_users_on_ci", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
 
 end
