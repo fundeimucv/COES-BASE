67c1ba20ad3e43ea1d6ca2a464d466882aff9750
Daniel Moros <danielousky@gmail.com>
2022-12-06 15:05:02 -0400

    Added Bank model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :banks
   resources :faculties
   resources :profiles
   resources :academic_processes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_184806) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_185854) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_184806) do
     t.datetime "updated_at", null: false
     t.index ["area_id"], name: "index_areas_on_area_id"
     t.index ["school_id"], name: "index_areas_on_school_id"
+  end
+
+  create_table "banks", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "faculties", force: :cascade do |t|
