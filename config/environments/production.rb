89d9d9265c524fca14d91af600d405fd70ef0c8c
Daniel Moros <danielousky@gmail.com>
2023-01-11 12:59:19 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
 
   config.serve_static_assets = false
-  config.assets.compress = true
+  # config.assets.compress = true
   config.assets.digest = true
 
   config.consider_all_requests_local       = false
@@ -48,10 +48,10 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
