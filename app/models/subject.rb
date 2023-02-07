c83399e322048a62da065804289c68d940687643
Daniel Moros <danielousky@gmail.com>
2023-02-07 17:18:31 -0400

    Update subject.rb

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -37,6 +37,7 @@ class Subject < ApplicationRecord
   
   # HOOKS:
   def clean_values
+    self.name.delete! '^0-9|^A-Za-z|áÁÄäËëÉéÍÏïíÓóÖöÚúÜüñÑ.() '
     self.name.strip!
     self.code.delete! '^0-9|^A-Za-z|áÁÄäËëÉéÍÏïíÓóÖöÚúÜüñÑ'
     self.code.strip!
