ae4ab054695b1e02e1cefdb7be4d39b096460fdf
Daniel Moros <danielito.moros03@gmail.com>
2023-02-22 10:27:58 -0400

    Update student_session_controller.rb

--- app/controllers/student_session_controller.rb
+++ app/controllers/student_session_controller.rb
@@ -3,5 +3,6 @@ class StudentSessionController < ApplicationController
 	before_action :set_session_id_if_multirols, only: [:dashboard]
 
 	def dashboard
+		# session[:student_id] ||= current_user.id
 	end
 end
