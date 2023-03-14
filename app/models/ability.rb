867344cfefbadc0f42e07079060d953b89797f0a
Daniel Moros <danielousky@gmail.com>
2023-03-14 06:12:36 -0400

    General Adjustment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -22,8 +22,8 @@ class Ability
       if user.admin.yo?
         can :manage, :all
       elsif user.admin.jefe_control_estudio?
-        can :manage, [Admin, Student, Teacher, Area, Subject, School, Bank, BankAccount, PaymentReport, Course, Grade, AcademicProcess, EnrollAcademicProcess, AcademicRecord, Section, AdmissionType, PeriodType, Address, StudyPlan]
-        can :crue, [User, Faculty]
+        can :manage, [Admin, Student, Teacher, Area, Subject, Bank, BankAccount, PaymentReport, Course, Grade, AcademicProcess, EnrollAcademicProcess, AcademicRecord, Section, AdmissionType, PeriodType, Address, StudyPlan, Period]
+        can :crue, [School, User, Faculty]
       else
         cannot :manage, [User, Admin, Student, Teacher, Area, Subject, School, Bank, BankAccount, PaymentReport, Course, Grade, AcademicProcess, EnrollAcademicProcess, AcademicRecord, Section, AdmissionType, PeriodType, Address]
       end
