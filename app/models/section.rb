c7a0de05d08ee67e7f9561b270103f2c7d5f07eb
Daniel Moros <danielousky@gmail.com>
2023-03-22 19:18:38 -0400

    Add translation message on action

--- app/models/section.rb
+++ app/models/section.rb
@@ -11,6 +11,10 @@ class Section < ApplicationRecord
   # HISTORY:
   has_paper_trail on: [:create, :destroy, :update]
 
+  before_create :paper_trail_create
+  before_destroy :paper_trail_destroy
+  before_update :paper_trail_update#, on: :update
+
   # ASSOCIATIONS:
   # belongs_to
   belongs_to :course
@@ -336,4 +340,23 @@ class Section < ApplicationRecord
     [total_newed, total_updated, no_registred]
   end
 
+  private
+
+
+    def paper_trail_update
+      # changed_fields = self.changes.keys - ['created_at', 'updated_at']
+      object = I18n.t("activerecord.models.#{self.model_name.param_key}.one")
+      # self.paper_trail_event = "¡#{object} actualizado en #{changed_fields.to_sentence}"
+      self.paper_trail_event = "#{object} actualizada."
+    end  
+
+    def paper_trail_create
+      object = I18n.t("activerecord.models.#{self.model_name.param_key}.one")
+      self.paper_trail_event = "¡#{object} registrada!"
+    end  
+
+    def paper_trail_destroy
+      object = I18n.t("activerecord.models.#{self.model_name.param_key}.one")
+      self.paper_trail_event = "¡#{object} eliminada!"
+    end
 end
