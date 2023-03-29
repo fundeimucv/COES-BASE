2b599597cf98a0d528003eb5d0585ea9d121208a
Daniel Moros <danielousky@gmail.com>
2023-03-29 15:32:06 -0400

    Include image_profile in enroll constance

--- app/models/codigo_qr.rb
+++ app/models/codigo_qr.rb
@@ -7,7 +7,7 @@ class CodigoQr
     qrcode = RQRCode::QRCode.new(link)
 
     png = qrcode.as_png(
-      bit_depth: 1,
+      bit_depth: 2,
       border_modules: 2,
       color_mode: ChunkyPNG::COLOR_GRAYSCALE,
       color: 'black',
@@ -16,7 +16,7 @@ class CodigoQr
       module_px_size: 6,
       resize_exactly_to: false,
       resize_gte_to: false,
-      size: 300
+      size: 180
     )
 
     return "#{Rails.root.to_s}/tmp/barcode.png"
