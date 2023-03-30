4877406e62ec2580750f70092a240bd0207394e2
Daniel Moros <danielousky@gmail.com>
2023-03-30 16:17:31 -0400

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "coes.fau@gmail.com"
+  default from: 'SOPORTE COES-FAU <soporte@coesfau.com>'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
