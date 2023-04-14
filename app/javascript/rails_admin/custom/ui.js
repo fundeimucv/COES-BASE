df254cacfad3bad537a844e3362bd71e68efeaba
Daniel Moros <danielousky@gmail.com>
2023-04-13 20:15:56 -0400

    Update Dashboard

--- app/javascript/rails_admin/custom/ui.js
+++ app/javascript/rails_admin/custom/ui.js
@@ -11,7 +11,7 @@ document.addEventListener("rails_admin.dom_ready", function() {
 	$('[rel="tooltip"]').tooltip();
 	$('[title!=""]').tooltip();
 	$('#update_if_exists').removeClass('form-control');
-	$('[data-bs-toggle="collapse"]').click();
+	// $('[data-bs-toggle="collapse"]').click();
 
 	$(".diplayModalBtn").on('click', function() {
 		var idModal = $(this).attr('idmodal');
