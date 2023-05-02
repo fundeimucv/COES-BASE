d8b0a4560b3dc406455bc2a719d68249e8d9c92f
Daniel Moros <danielousky@gmail.com>
2023-05-02 18:40:12 -0400

    Create schema_authentication_load.rb

---
+++ schema_authentication_load.rb
@@ -0,0 +1,46 @@
+Admin PARE  solo desarrolladores y JCE
+
+'Gestión Academica' 'fa-regular fa-school-flag'
+ School 'fa-regular fa-school'
+
+ AdmissionType 'fa-regular fa-user-tag'
+
+ StudyPlan 'fa-solid fa-award'
+
+ Subject 'fa-regular fa-book'
+  Area
+  Dependency
+
+'Gestión de Usuarios' 'fa-regular fa-users'
+Student 'fa-regular fa-user-graduate'
+ User
+ Address
+ Grade
+ AdmissionType (only: Show)
+  (only: Show)
+
+Teacher 'fa-regular fa-chalkboard-user'
+ User
+ 
+'Gestión Periódica' 'fa-solid fa-book-open-reader'
+AcademicProcess 'fa-solid fa-calendar'
+ Period
+  PeriodType
+
+Course 'fa-regular fa-shapes'
+
+Section 'fa-regular fa-list'
+ Schedule
+
+EnrollAcademicProcess 'fa-regular fa-calendar-check'
+
+AcademicRecord 'fa-regular fa-signature'
+Qualification 'fa-regular fa-list-check'
+
+'Informativos' 'fa-regular fa-info'
+ Billboard 'fa-regular fa-panorama'
+
+---
+ Bank
+ BankAccount
+ PaymentReport
