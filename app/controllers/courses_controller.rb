13acdd2bb0066c523e38f45bde871105bf45d1d0
Daniel Moros <danielousky@gmail.com>
2023-06-23 10:45:08 -0400

    Adjustment in view in Programation

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,28 +1,39 @@
 class CoursesController < ApplicationController
 
   def create
-    respond_to do |format|
-      if course = Course.create(course_params)
-        new_section = ApplicationController.helpers.button_add_section(course.id)
-        # p "   CURSO: <#{new_section}>.   ".center(500, "#")
-        format.json {render json: {data: "¡Curso activado para el período #{course.academic_process.period_name}!", status: :success, new_section: new_section, type: :create} }
-      else
-        format.json { render json: {data: course.errors, status: :unprocessable_entity} }
+    begin
+      respond_to do |format|
+        course = Course.new(course_params)
+        if course.save
+          new_section = ApplicationController.helpers.button_add_section(course.id)
+          # p "   CURSO: <#{new_section}>.   ".center(500, "#")
+          format.json {render json: {data: "¡Curso activado para el período #{course.academic_process.period_name}!", status: :success, new_section: new_section, type: :create} }
+        else
+          format.json { render json: {data: course.errors, status: :unprocessable_entity} }
+        end
       end
+      
+    rescue Exception => e
+      format.json { render json: {data: e, status: :unprocessable_entity} }
     end
   end
 
   # DELETE /courses/1 or /courses/1.json
   def destroy
+    begin
+      course = Course.find_by(course_params)
+      period_name = course.academic_process.period_name
 
-    course = Course.find_by(course_params)
-    period_name = course.academic_process.period_name
-
-    respond_to do |format|
-      if course.destroy
-        format.json {render json: {data: "¡Curso desactivado para el período #{period_name}!", status: :success, type: :destroy} }
-      else
-        format.json {render json: {data: "¡Error al intentar desactivar la asignatura para el período #{period_name}!", status: :unprocessable_entity} }
+      respond_to do |format|
+        if course.destroy
+          format.json {render json: {data: "¡Curso desactivado para el período #{period_name}!", status: :success, type: :destroy} }
+        else
+          format.json {render json: {data: "¡Error al intentar desactivar la asignatura para el período #{period_name}!", status: :unprocessable_entity} }
+        end
+      end
+    rescue Exception => e
+      respond_to do |format|
+        format.json {render json: {data: e, status: :unprocessable_entity} }
       end
     end
   end
