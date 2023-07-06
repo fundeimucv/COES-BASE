835b0e15b012a48bc3102e69faaa140ce6d1774c
Daniel Moros <danielousky@gmail.com>
2023-07-06 17:54:55 -0400

    Update passwords_controller.rb

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -25,9 +25,9 @@ class PasswordsController < Devise::PasswordsController
   end
 
   # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #  super
-  # end
+  def edit
+   super
+  end
 
   # PUT /resource/password
   # def update
