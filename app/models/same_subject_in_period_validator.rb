2847c131a1fe4b83447fe3bd0a1084adb4b7306d
Daniel Moros <danielousky@gmail.com>
2023-07-18 12:51:30 -0400

    Update Programation
    issue bug: show sections in list of subjects

--- app/models/same_subject_in_period_validator.rb
+++ app/models/same_subject_in_period_validator.rb
@@ -1,7 +1,7 @@
 class SameSubjectInPeriodValidator < ActiveModel::Validator
   def validate(record)
     if same_course_in_period(record)
-      record.errors.add "#{record.subject.name.upcase} ya fue inscrita", "en éste período (#{record.enroll_academic_process.period.name})"
+      record.errors.add "Ya inscrita asignatura: #{record.subject.name.upcase}", "en éste período (#{record.enroll_academic_process.period.name})"
     end
   end
 
