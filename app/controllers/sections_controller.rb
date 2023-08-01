f39b1c571478bd77d9468cd4a885c197e915177c
Daniel Moros <danielousky@gmail.com>
2023-08-01 18:39:46 -0400

    updated acta

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -29,7 +29,9 @@ class SectionsController < ApplicationController
       respond_to do |format|
         format.html
         format.pdf do
-          render pdf: "acta_#{@section.number_acta}", template: "sections/acta", locals: {section: @section}, formats: [:html], page_size: 'letter', header: {html: {template: '/sections/acta_header', formats: [:html], layout: false, locals: {school: @section.school, section: @section}}}, footer: {html: {template: '/sections/signatures', formats: [:html]}}, margin: {top: 78, bottom: 68}#, dpi: 150
+          top = 72
+          top = 80 if @section.subject&.name&.length > 53
+          render pdf: "acta_#{@section.number_acta}", template: "sections/acta", locals: {section: @section}, formats: [:html], page_size: 'letter', header: {html: {template: '/sections/acta_header', formats: [:html], layout: false, locals: {school: @section.school, section: @section}}}, footer: {html: {template: '/sections/signatures', formats: [:html]}}, margin: {top: top, bottom: 68}#, dpi: 150
         end
       end
     else
