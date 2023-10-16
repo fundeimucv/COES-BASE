e06367aac02dd192cbfb72dbded5b4d1024c807c
Daniel Moros <danielousky@gmail.com>
2023-10-16 12:48:21 -0400

    Advancing in export stream

--- lib/rails_admin/config/actions/custom_export.rb
+++ lib/rails_admin/config/actions/custom_export.rb
@@ -20,29 +20,31 @@ module RailsAdmin
 
         register_instance_option :controller do
           proc do
-            if request.get?
-              render action: @action.template_name
+            begin
+              if request.get?
+                render action: @action.template_name
 
-            elsif request.post?
-              from = Time.zone.parse(params[:from])
+              elsif request.post?
+                from = Time.zone.parse(params[:from])
 
-              # Prepare headers for streaming
-              response.headers.delete('Content-Length')
-              response.headers['Cache-Control'] = 'no-cache'
-              response.headers['X-Accel-Buffering'] = 'no'
-              response.headers['Content-Type'] = 'text/event-stream'
+                # Prepare headers for streaming
+                response.headers.delete('Content-Length')
+                response.headers['Cache-Control'] = 'no-cache'
+                response.headers['X-Accel-Buffering'] = 'no'
+                response.headers['Content-Type'] = 'text/event-stream'
 
-              # There's an issue in rack where ActionController::Live doesn't work with the ETags middleware
-              # See https://github.com/rack/rack/issues/1619#issuecomment-606315714
-              response.headers['ETag'] = '0'
-              response.headers['Last-Modified'] = '0'
+                # There's an issue in rack where ActionController::Live doesn't work with the ETags middleware
+                # See https://github.com/rack/rack/issues/1619#issuecomment-606315714
+                response.headers['ETag'] = '0'
+                response.headers['Last-Modified'] = '0'
 
-              # Download response stream into a file
-              response.headers['Content-Disposition'] = "attachment; filename=testfile.txt"
+                # Download response stream into a file
+                response.headers['Content-Disposition'] = "attachment; filename=testfile.txt"
 
-              SomeModel.where('created_at > ?', from).find_each(batch_size: 500) do |record|
-                # Define how the records should be exported
-                response.stream.write record.to_s
+                AcademicRecord.where('created_at > ?', from).find_each(batch_size: 500) do |record|
+                  # Define how the records should be exported
+                  response.stream.write record.to_s
+                end
               end
             ensure
               response.stream.close
--- lib/rails_admin/config/actions/export.rb
+++ lib/rails_admin/config/actions/export.rb
@@ -32,6 +32,34 @@ module RailsAdmin
           end
         end
 
+
+        # register_instance_option :controller do
+        #   proc do
+
+        #     begin
+        #       format = params[:json] && :json || params[:csv] && :csv || params[:xml] && :xml
+        #       if format
+        #         response.headers.delete('Content-Length')
+        #         response.headers['Cache-Control'] = 'no-cache'
+        #         response.headers['X-Accel-Buffering'] = 'no'
+        #         response.headers['Content-Type'] = 'text/event-stream'
+        #         response.headers['ETag'] = '0'
+        #         response.headers['Last-Modified'] = '0'
+        #         aux = "Reporte Coes #{I18n.t("activerecord.models.#{params[:model_name]}.other")&.titleize} #{DateTime.now.strftime('%d-%m-%Y_%I:%M%P')}.xls"
+        #         response.headers['Content-Disposition'] = "attachment; filename=#{aux}"
+        #         request.format = format
+        #         @schema = HashHelper.symbolize(params[:schema].slice(:except, :include, :methods, :only).permit!.to_h) if params[:schema] # to_json and to_xml expect symbols for keys AND values.
+        #         @objects = list_entries(@model_config, :export)
+        #         response.stream.write index
+                
+        #       end
+        #     ensure
+        #       response.stream.close
+        #     end
+        #   end
+        # end
+
+
         register_instance_option :bulkable? do
           true
         end
