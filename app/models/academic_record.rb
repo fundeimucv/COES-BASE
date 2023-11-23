10653f99d6470294cdfe94366070f9af83f16c14
Daniel Moros <danielousky@gmail.com>
2023-11-23 12:23:34 -0400

    Update academic_record.rb

--- app/models/academic_record.rb
+++ app/models/academic_record.rb
@@ -49,7 +49,7 @@ class AcademicRecord < ApplicationRecord
   # validates :qualifications, presence: true, if: lambda{ |object| (object.subject.present? and object.subject.numerica? and (object.aprobado? or object.aplazado? or object.equivalencia? ))}
 
   # OJO: Se usó este validador en luegar del de arriba para poder espeficificar el mensaje
-  validates_presence_of :qualifications, message: "Calificación no puede estar en blanco. Si desea eliminar la calificación, coloque el estado de calificación a 'Sin Calificar'", if: lambda{ |object| (object.subject.present? and object.subject.numerica? and (object.aprobado? or object.aplazado? or object.equivalencia?))}
+  validates_presence_of :qualifications, message: "Calificación no puede estar en blanco. Si desea eliminar la calificación, coloque el estado de calificación a 'Sin Calificar'", if: lambda{ |object| (object.subject.present? and object.subject.numerica? and (object.aprobado? or object.aplazado?))}
 
   # CALLBACK
   after_save :set_options_q
