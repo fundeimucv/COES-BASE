9be6318867f02029633676e9a81f4123bd32902d
Daniel Moros <danielousky@gmail.com>
2024-06-19 18:39:35 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 #  last_name              :string
 #  last_sign_in_at        :datetime
 #  last_sign_in_ip        :string
+#  location_number_phone  :string
 #  number_phone           :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
