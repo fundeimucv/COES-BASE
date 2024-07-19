674d7f32f60c6b5cf41a16b16bd74896c855b148
Daniel Moros <danielousky@gmail.com>
2024-07-19 19:59:49 -0400

    Complete updated UI in Admin
    Update sidebar, nav, content, and others

--- app/javascript/rails_admin/custom/ui.js
+++ app/javascript/rails_admin/custom/ui.js
@@ -3,12 +3,14 @@
 import * as toastr from "toastr";
 window.toastr = toastr;
 
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
 // import "trix"
 import "@rails/actiontext"
 
 document.addEventListener("rails_admin.dom_ready", function() {
 	// $('[rel="tooltip"]').tooltip();
-	$('[title!=" "]').tooltip();
+	// $('[title!=" "]').tooltip();
 	$('#update_if_exists').removeClass('form-control');
 	// $('[data-bs-toggle="collapse"]').click();
 
@@ -22,3 +24,7 @@ document.addEventListener("rails_admin.dom_ready", function() {
 	
 });
 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
