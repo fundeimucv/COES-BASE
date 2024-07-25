5bff89a9fb0215db7d1e913fb16e6a7999431ea3
Daniel Moros <danielousky@gmail.com>
2024-07-25 16:05:50 -0400

    Update domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "plus.coesfhe.com" }
+  config.action_mailer.default_url_options = { host: "coesfhe.com" }
   
   # Settings specified here will take precedence over those in config/application.rb.
 
