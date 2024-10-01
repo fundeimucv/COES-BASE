aaa97041194ace8ac97b645a234b7745f7e1cb56
Daniel Moros <danielousky@gmail.com>
2024-10-01 16:38:16 -0400

    Update historicals form

--- app/controllers/academic_records_controller.rb
+++ app/controllers/academic_records_controller.rb
@@ -28,7 +28,8 @@ class AcademicRecordsController < ApplicationController
     params[:academic_record][:status] = params[:academic_record][:status].delete(" ").underscore.to_sym
     params[:academic_record][:status] = 'sin_calificar' if (params[:academic_record][:status].to_s.eql? 'calificar')
     params[:academic_record][:status] = 'perdida_por_inasistencia' if (params[:academic_record][:status].to_s.eql? 'pérdidapor_inasistencia') 
-    params[:academic_record][:status] = 'aprobado' if (params[:academic_record][:status].to_s.eql? 'abrobada') 
+    # params[:academic_record][:status] = 'aprobado' if (params[:academic_record][:status].to_s.eql? 'Aprobada') 
+    # params[:academic_record][:status] = 'aplazado' if (params[:academic_record][:status].to_s.eql? 'Aplazada') 
 
     @academic_record = AcademicRecord.new(academic_record_params)
 
